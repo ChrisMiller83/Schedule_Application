c18188d18251f948c4898a322dc33c25b5f6099e
ChrisMiller <cmil788@wgu.edu>
2022-08-23 11:52:37 -0600

    Lab locking up again

--- src/DAO/ContactDAO.java
+++ src/DAO/ContactDAO.java
@@ -1,5 +1,11 @@
 package DAO;
 
+import model.Contact;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ContactDAO {
 
 
@@ -20,7 +26,26 @@ public class ContactDAO {
     public static final int INDEX_CONTACT_EMAIL = 3;
 
     public static final String QUERY_ALL_CONTACTS = "SELECT * FROM " + TABLE_CONTACTS;
-    public static final String
+
+
+    public static void loadAllContacts() {
+        try {
+            PreparedStatement loadContacts = DBConnection.getConnection().prepareStatement(QUERY_ALL_CONTACTS);
+            ResultSet result = loadContacts.executeQuery();
+
+            while (result.next()) {
+                int contactId = result.getInt(COLUMN_CONTACT_ID);
+                String contactName = result.getString(COLUMN_CONTACT_NAME);
+                String email = result.getString(COLUMN_CONTACT_EMAIL);
+                Contact contact = new Contact(contactId, contactName, email);
+                Contact.contacts.add(contact);
+            }
+
+            // add reports for contacts
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
