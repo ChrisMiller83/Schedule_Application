2f031e07263e839c3ae4533bdb7a88b8e01c96df
ChrisMiller <cmil788@wgu.edu>
2022-08-25 16:12:41 -0600

    redesigned addApptView.fxml and its controller

--- src/controller/addApptController.java
+++ src/controller/addApptController.java
@@ -1,18 +1,40 @@
 package controller;
 
+import DAO.CustomerDAO;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.Contact;
+import model.Customer;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class addApptController implements Initializable {
+
+    @FXML private TextField appointmentIdTF;
+    @FXML private ChoiceBox<Customer> customerCB;
+    @FXML private TextField titleTF;
+    @FXML private TextField locationTF;
+    @FXML private TextField typeTF;
+    @FXML private ChoiceBox<Contact> contactCB;
+    @FXML private DatePicker startDatePicker;
+    @FXML private ChoiceBox startTimeCB;
+    @FXML private DatePicker endDatePicker;
+    @FXML private ChoiceBox endTimeCB;
+    @FXML private TextArea descriptionTA;
+    @FXML private Button cancelBtn;
+    @FXML private Button saveBtn;
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
